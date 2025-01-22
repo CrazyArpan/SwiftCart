062900e4d8d4968c79bbc7ab118822edc049aedc
CrazyArpan <166271755+CrazyArpan@users.noreply.github.com>
2025-01-22 10:40:11 +0530

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,16 +1,19 @@
-import { ClerkMiddleware } from "@clerk/nextjs/server";
-import { auth } from '@clerk/nextjs/server'
+import { clerkMiddleware } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
 
-export default ClerkMiddleware({
+export default clerkMiddleware({
   publicRoutes: ["/", "/api/webhook/clerk"],
-  ignoredRoutes: ["/api/orders/update-status"]
+  ignoredRoutes: ["/api/orders/update-status"],
+  afterAuth(auth, req) {
+    if (!auth.userId && !auth.isPublicRoute) {
+      const signInUrl = new URL('/sign-in', req.url);
+      signInUrl.searchParams.set('redirect_url', req.url);
+      return NextResponse.redirect(signInUrl);
+    }
+    return NextResponse.next();
+  }
 });
 
 export const config = {
-  matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    // Always run for API routes
-    '/(api|trpc)(.*)',
-  ],
-}
+  matcher: ["/(.*)", "/"]
+};
